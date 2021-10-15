769a8ad277cdfce0eb8938b7ae8b317f9ed694b6
Lucas Pontes <lucasdev@pop-os.localdomain>
2021-10-15 09:51:21 -0300

    Criando a primeira classe.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace poo_bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 893;
+            contaDaGabriela.numero = 893;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+        }
+    }
+}
