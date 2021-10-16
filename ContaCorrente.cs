1b7646431417d9d2f63ab3d4465085b83cabf57b
Lucas Pontes <lucasdev@pop-os.localdomain>
2021-10-16 10:34:35 -0300

    Criado as funçõe sde ObterSaldo e DefinirSaldo.

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -10,8 +10,19 @@ namespace poo_bytebank
         public Cliente titular;
         public int agencia;
         public int numero;
-        public double saldo;
+        private double saldo;
         
+        public void DefinirSaldo(double saldo){
+            if(saldo < 0){
+                return;
+            }
+            this.saldo = saldo;
+        }
+
+        public double ObterSaldo(){
+            return saldo;
+        }
+
         public bool Sacar(double valor){
             if(this.saldo < valor){
                 return false;
--- Program.cs
+++ Program.cs
@@ -47,10 +47,11 @@ namespace poo_bytebank
 
             contaGabriela.titular = gabriela;
             //contaGabriela.titular = new Cliente();
-            contaGabriela.saldo = 500;
+            contaGabriela.DefinirSaldo(500);
             contaGabriela.agencia = 5555;
             contaGabriela.numero = 065;
 
+            Console.WriteLine("Saldo cliente: " + contaGabriela.ObterSaldo());
             Console.WriteLine("Objeto cliente: " + gabriela.nome);
             Console.WriteLine("Obejto conta corrente: " + contaGabriela.titular.nome);
 
